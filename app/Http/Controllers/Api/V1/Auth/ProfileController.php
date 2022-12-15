7085c6496add57179e72d286fb1f80e22b88b64b
PovilasKorop <povilask007@gmail.com>
2022-12-15 19:36:33 +0200

    Profile endpoints

---
+++ app/Http/Controllers/Api/V1/Auth/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        return response()->json([
+            'name' => auth()->user()->name,
+            'email' => auth()->user()->email,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', Rule::unique('users')->ignore(auth()->user())]
+        ]);
+
+        auth()->user()->update($validatedData);
+
+        return response()->json($validatedData);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+});
+
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
