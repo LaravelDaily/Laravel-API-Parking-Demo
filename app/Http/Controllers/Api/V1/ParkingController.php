680191e9317f4aa3880e3beeb3cde4fa9008764f
David Lun <mysticcode@gmail.com>
2023-01-03 14:37:34 +0200

    Fix bug where user can book parking for cars he doesn't own by spoofing vehicle id or car is removed from account

--- app/Http/Controllers/Api/V1/ParkingController.php
+++ app/Http/Controllers/Api/V1/ParkingController.php
@@ -31,7 +31,11 @@ class ParkingController extends Controller
     public function start(Request $request)
     {
         $parkingData = $request->validate([
-            'vehicle_id' => ['required', 'integer', 'exists:vehicles,id'],
+            'vehicle_id' => [
+                'required',
+                'integer',
+                'exists:vehicles,id,deleted_at,NULL,user_id,'.auth()->id(),
+            ],
             'zone_id'    => ['required', 'integer', 'exists:zones,id'],
         ]);
 
